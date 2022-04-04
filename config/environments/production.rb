1a519ae05c8ae2fc7aaa3cb411f4681c1307edd8
Noman Ahmad <noman.ahmad456@gmail.com>
2022-04-04 17:39:46 +0530

    jquery error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,6 +5,7 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
+  Rails.application.routes.default_url_options = { host: "https://radiant-citadel-74262.herokuapp.com/" }
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
